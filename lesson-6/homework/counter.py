a22a32c5a60e3ba3cc4a13a8fcbf61ce53036210
Musobek2007 <rahimberganovmusobek@gmail.com>
2025-06-03 18:38:56 +0500

    Hello

--- lesson-6/homework/counter.py
+++ lesson-6/homework/counter.py
@@ -12,10 +12,18 @@ p=Counter(lis)
 lis=list(set(lis))
 lis.sort(key=lambda x:p[x],reverse=1)
 rank=int(input('Enter number of top common words: '))
+txt=open('word_count_report.txt','w')
 print('Total words:',len(lis))
+txt.write('Total words: '+str(len(lis)))
 print('Top',rank,'most common words:')
+txt.write('\n')
+txt.write('Top '+str(rank)+' most common words:')
+txt.write('\n')
 n=0
 for i in lis:
     if n>=rank:break
     print(i,'-',p[i],'times')
+    txt.write(i+'-'+str(p[i])+' times')
+    txt.write('\n')
     n+=1
+txt.close()
